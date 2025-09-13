f4047efcc613e1d0a670a91a06b2e3e638cbff89
mtctx <me@mtctx.dev>
2025-09-13 23:33:03 +0200

    Remove `update_readme_version.yml` workflow and add copyright headers across project files

--- src/main/kotlin/dev/mtctx/unipub/UniPub.kt
+++ src/main/kotlin/dev/mtctx/unipub/UniPub.kt
@@ -1,6 +1,6 @@
 /*
  *     UniPub: UniPub.kt
- *     Copyright (C) 2025  mtctx
+ *     Copyright (C) 2025 mtctx
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
